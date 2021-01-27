ff8173a2c547b27167f03ae32873f59817a46908
Bertrand2 <dossogne.bertrand@gmail.com>
2021-01-27 12:07:56 +0100

    first commit

---
+++ Counter.js
@@ -0,0 +1,14 @@
+"use strict";
+
+import React, { Component } from "react";
+
+
+export default class Counter extends Component {
+	state = { count: 0 };
+
+	incrementCount = () => this.setState({ count: this.state.count + 1 });
+
+	render() {
+	return (<h1 onClick={this.incrementCount}>YOLO : {this.state.count}</h1>)
+	}
+}
